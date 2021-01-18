d6a17a1c77e9454d57bd4503a1d34e025733f2e7
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-01-18 18:22:33 +0100

    create routs.js file

---
+++ routs.js
@@ -0,0 +1,27 @@
+weatherApp.config(['$routeProvider', '$locationProvider', '$sceDelegateProvider', function ($routeProvider, $locationProvider, $sceDelegateProvider) {
+    
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        // Allow loading from our assets domain. **.
+        'http://api.openweathermap.org/**'
+      ]);
+
+    $locationProvider.hashPrefix('');
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'  
+    })
+
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'  
+    })
+}])
