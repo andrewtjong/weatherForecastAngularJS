3847b6e8fbe8ef2f7b2efc277fca467b06b6da3d
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-01-18 18:28:19 +0100

    split and rearrange code

---
+++ controllers.js
@@ -0,0 +1,27 @@
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    })
+
+}]);
+
+
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', '$routeParams', function($scope, $resource, cityService, $routeParams) {
+
+    $scope.city = cityService.city;
+
+    $scope.days = $routeParams.days || 7;
+
+    $scope.weatherAPI= $resource("http://api.openweathermap.org/data/2.5/forecast?appid=bf80bdbfe08da086a652c60e3d9b12b5&units=metric", {get: { method: "JSONP"}})
+
+        $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+
+        console.log($scope.weatherResult)
+
+        $scope.convertToDate = function(dt) {
+            return new Date(dt * 1000)
+        }
+}]);
